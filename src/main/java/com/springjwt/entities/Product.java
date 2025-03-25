8b42c15ed9ccb905d19e46e78d58ad1a5e87c676
Guantai Bruce Muthomi <guantai.muthomi@MDGF01BE6B.local>
2025-03-25 21:02:50 +0300

    added product entity and its crud methods all secured by spring security

---
+++ src/main/java/com/springjwt/entities/Product.java
@@ -0,0 +1,46 @@
+package com.springjwt.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Double price;
+
+    public Product() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
